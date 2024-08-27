836142f9491fd7ab16fd08b41c1f6fa1cf293487
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-08-27 17:57:39 +0530

    Just a basic Create Progress Bar Plugin Created

--- main.ts
+++ main.ts
@@ -1,134 +1,94 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class CountCheckboxesPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "insert-checkbox-progress-bar",
+			name: "Insert Checkbox Progress Bar for Vault",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				this.insertCheckboxProgressBarForVault(editor);
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: "insert-checkbox-progress-bar-current-file",
+			name: "Insert Checkbox Progress Bar for Current File",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.insertCheckboxProgressBarForCurrentFile(editor, view);
+			},
+		});
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+	async insertCheckboxProgressBarForVault(editor: Editor) {
+		const files = this.app.vault.getMarkdownFiles();
+		let uncheckedCount = 0;
+		let checkedCount = 0;
+
+		for (const file of files) {
+			const content = await this.app.vault.cachedRead(file);
+
+			// Match unchecked boxes (- [ ])
+			const uncheckedMatches = content.match(/- \[ \]/g);
+			if (uncheckedMatches) {
+				uncheckedCount += uncheckedMatches.length;
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			// Match checked boxes (- [x])
+			const checkedMatches = content.match(/- \[x\]/gi);
+			if (checkedMatches) {
+				checkedCount += checkedMatches.length;
+			}
+		}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		const totalCheckboxes = uncheckedCount + checkedCount;
+		const progress =
+			totalCheckboxes > 0 ? (checkedCount / totalCheckboxes) * 100 : 0;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const progressBar =
+			`<div class="progress-container">
+                           <progress value="${progress}" max="100"></progress>
+                         </div>\n` +
+			`Checked: ${checkedCount} / Total: ${totalCheckboxes} (${progress.toFixed(
+				2
+			)}%)`;
+
+		const cursorPos = editor.getCursor();
+		editor.replaceRange(progressBar, cursorPos);
 	}
 
-	onunload() {
+	async insertCheckboxProgressBarForCurrentFile(
+		editor: Editor,
+		view: MarkdownView
+	) {
+		const content = view.data;
+		let uncheckedCount = 0;
+		let checkedCount = 0;
 
-	}
+		// Match unchecked boxes (- [ ])
+		const uncheckedMatches = content.match(/- \[ \]/g);
+		if (uncheckedMatches) {
+			uncheckedCount += uncheckedMatches.length;
+		}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+		// Match checked boxes (- [x])
+		const checkedMatches = content.match(/- \[x\]/gi);
+		if (checkedMatches) {
+			checkedCount += checkedMatches.length;
+		}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+		const totalCheckboxes = uncheckedCount + checkedCount;
+		const progress =
+			totalCheckboxes > 0 ? (checkedCount / totalCheckboxes) * 100 : 0;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		const progressBar =
+			`<div class="progress-container">
+                           <progress value="${progress}" max="100"></progress>
+                         </div>\n` +
+			`Checked: ${checkedCount} / Total: ${totalCheckboxes} (${progress.toFixed(
+				2
+			)}%)`;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const cursorPos = editor.getCursor();
+		editor.replaceRange(progressBar, cursorPos);
 	}
 }
